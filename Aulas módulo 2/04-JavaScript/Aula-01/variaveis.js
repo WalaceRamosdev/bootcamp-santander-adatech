9fb162fed0c219727af78e375b3cc2434b6a9537
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-11-05 22:53:05 -0300

    js03

---
+++ Aulas módulo 2/04-JavaScript/Aula-01/variaveis.js
@@ -0,0 +1,17 @@
+// variaveis
+
+let idade = 26 //criando a primeira variavel
+
+console.log(idade)
+
+//tipos de variaveis
+
+let numero = 30    //number
+let altura = 1.77  //numbert
+let nome = "Walace Toledo Ramos" //string
+let estudando = true // booleano
+
+console.log(numero)
+console.log(altura)
+console.log(nome)
+console.log(estudando)
