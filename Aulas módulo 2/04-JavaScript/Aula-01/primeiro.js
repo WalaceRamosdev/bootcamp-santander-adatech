576e43ab79ff089647b850a62ab6a6c148e6d2f2
Walace Ramos <contatowalace.dev@gmail.com>
2023-11-06 21:34:38 -0300

    Atualizar o primeiro.js

--- Aulas módulo 2/04-JavaScript/Aula-01/primeiro.js
+++ Aulas módulo 2/04-JavaScript/Aula-01/primeiro.js
@@ -1,4 +1,6 @@
 //Estou imprimindo duas mensagens no terminal
+
 console.log('Hello,World!')
 console.log('Bem vindo ao curso de JavaScript!')
+
 //Dia 6 de novembro não vou editado nenhuma linha de código 
