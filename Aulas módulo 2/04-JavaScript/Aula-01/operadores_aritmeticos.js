a85133a6f9ae06ec5ebcfd1fb2a09ed87cf18ceb
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-11-13 15:29:45 -0300

    js

--- Aulas módulo 2/04-JavaScript/Aula-01/operadores_aritmeticos.js
+++ Aulas módulo 2/04-JavaScript/Aula-01/operadores_aritmeticos.js
@@ -3,8 +3,13 @@
 let numero1 = 20
 let numero2 = 2
 
-// Adição          +
-// Subtração       -
-// Multiplicação   *
-// Divisão         /
-// Exponenciação   **
+// Adição            +
+// Subtração         -
+// Multiplicação     *
+// Divisão           /
+// Exponenciação     **
+// Resto da divisão  %
+
+
+// Precedência de operadores
+// Biblioteca Math
