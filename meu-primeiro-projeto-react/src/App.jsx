bb584e9a94c9c1db27c1727126fd69d023bb0a4e
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-11-14 20:47:41 -0300

    react

--- meu-primeiro-projeto-react/src/App.jsx
+++ meu-primeiro-projeto-react/src/App.jsx
@@ -6,12 +6,7 @@ import viteLogo from '/vite.svg'
 function App() {
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  
   )
 }
 
