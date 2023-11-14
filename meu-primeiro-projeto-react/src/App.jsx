2ac8355d725eda37087030e593d021079344b822
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-11-14 20:49:19 -0300

    react

--- meu-primeiro-projeto-react/src/App.jsx
+++ meu-primeiro-projeto-react/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
 function App() {
 
   return (
-  
+  <h1>Olá mundo, REACT !</h1>
   )
 }
 
