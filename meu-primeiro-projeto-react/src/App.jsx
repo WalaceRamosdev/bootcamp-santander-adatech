5da8e65d741eda06c3ce5cad5bd69a73010d52ce
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-11-15 11:35:25 -0300

    react

--- meu-primeiro-projeto-react/src/App.jsx
+++ meu-primeiro-projeto-react/src/App.jsx
@@ -3,7 +3,9 @@ function App() {
   return (
   <div>
     <h1>Olá mundo, REACT !</h1>
-    <MeuComponente></MeuComponente>
+    <MeuComponente/>
+    <MeuComponente/>
+    <MyButton/>
   </div>
   )
 }
@@ -11,6 +13,12 @@ function App() {
 function MeuComponente() {
   return (
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, voluptas qui nihil deleniti quo voluptatem, ad, numquam cum aut consequuntur quidem cumque similique unde alias totam. Quibusdam suscipit nihil sit?</p>
+  )
+}
+
+function MyButton() {
+  return(
+  <button>Clique aqui</button>
   )
 }
 
